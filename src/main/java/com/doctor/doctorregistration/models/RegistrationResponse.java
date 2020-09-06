ae28673d3b7247c9a9dc4c3dc65e3dedf9150a86
Sachin <sachinkmr375@gmail.com>
2020-09-06 15:38:42 +0530

    Backup

--- src/main/java/com/doctor/doctorregistration/models/RegistrationResponse.java
+++ src/main/java/com/doctor/doctorregistration/models/RegistrationResponse.java
@@ -1,2 +1,19 @@
-package com.doctor.doctorregistration.models;public class RegistrationResponse {
+package com.doctor.doctorregistration.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+public class RegistrationResponse {
+    private int doctorId;
+
+    public RegistrationResponse() {
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
 }
